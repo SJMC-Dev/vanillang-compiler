beae608ed682570017617c05097c8a77a9ffcbe6
Renvle <47477363+renvlers@users.noreply.github.com>
2026-09-26 17:20:43 +0800

    test(parser): cover generic arguments in identifier-like expressions

--- tests/ParserTest.cpp
+++ tests/ParserTest.cpp
@@ -1,6 +1,8 @@
 #include "parser/Parser.hpp"
 #include "ast/declaration/FunctionDeclarationNode.hpp"
+#include "ast/expression/BinaryExpressionNode.hpp"
 #include "ast/expression/IdentifierLikeExpressionNode.hpp"
+#include "ast/expression/MemberAccessExpressionNode.hpp"
 #include "ast/expression/NoneExpressionNode.hpp"
 #include "ast/expression/PrimitiveTypeExpressionKind.hpp"
 #include "ast/expression/PrimitiveTypeExpressionNode.hpp"
@@ -236,6 +238,165 @@ namespace vnlc {
         EXPECT_EQ(customizedReference->getNameParts().front()->getIdentifierString(), "Example");
         EXPECT_FALSE(customizedReference->hasQuestionMarkSuffix());
         EXPECT_TRUE(customizedReference->getGenericArguments().empty());
+    }
+
+    TEST(ParserTest, ParsesGenericArgumentsForLocalTypeDeclarationsOnly) {
+        std::stringstream input(
+            "class Box {}\n"
+            "class Pair {}\n"
+            "class Item {}\n"
+            "func test() {\n"
+            "    let generic = Box<Pair<Item>>\n"
+            "    let member = Box<Item.value>\n"
+            "    let compared = left < right\n"
+            "}\n"
+        );
+
+        Config config{
+            .mode = RunningMode::COMPILE,
+            .vanillangVersion = "1.0",
+            .minecraftVersion = "26.1.2",
+            .packageRootPath = std::filesystem::current_path(),
+            .inputFilePath = std::filesystem::current_path() / "test.vnl",
+            .outputDirectory = std::nullopt,
+            .dependencyPackageRootPaths = {},
+            .optimizationLevel = std::nullopt,
+        };
+
+        auto result = parseModule(input, config);
+        const auto& module = result.getModuleNode();
+        ASSERT_EQ(module.getTopIdentifierDeclarations().size(), 4);
+
+        const auto* function = dynamic_cast<const FunctionDeclarationNode*>(module.getTopIdentifierDeclarations()[3].get());
+        ASSERT_NE(function, nullptr);
+        ASSERT_TRUE(function->getBody().has_value());
+
+        const auto& statements = function->getBody().value()->getStatements();
+        ASSERT_EQ(statements.size(), 3);
+
+        const auto* genericStatement = dynamic_cast<const VariableDeclarationStatementNode*>(statements[0].get());
+        ASSERT_NE(genericStatement, nullptr);
+        ASSERT_TRUE(genericStatement->getVariableDeclaration().getInitializer().has_value());
+        const auto* genericExpression = dynamic_cast<const IdentifierLikeExpressionNode*>(genericStatement->getVariableDeclaration().getInitializer().value().get());
+        ASSERT_NE(genericExpression, nullptr);
+        EXPECT_EQ(genericExpression->getName().getIdentifierString(), "Box");
+        ASSERT_EQ(genericExpression->getGenericArguments().size(), 1);
+        const auto* pairExpression = dynamic_cast<const IdentifierLikeExpressionNode*>(genericExpression->getGenericArguments().front().get());
+        ASSERT_NE(pairExpression, nullptr);
+        EXPECT_EQ(pairExpression->getName().getIdentifierString(), "Pair");
+        ASSERT_EQ(pairExpression->getGenericArguments().size(), 1);
+        const auto* itemExpression = dynamic_cast<const IdentifierLikeExpressionNode*>(pairExpression->getGenericArguments().front().get());
+        ASSERT_NE(itemExpression, nullptr);
+        EXPECT_EQ(itemExpression->getName().getIdentifierString(), "Item");
+        EXPECT_TRUE(itemExpression->getGenericArguments().empty());
+
+        const auto* memberStatement = dynamic_cast<const VariableDeclarationStatementNode*>(statements[1].get());
+        ASSERT_NE(memberStatement, nullptr);
+        ASSERT_TRUE(memberStatement->getVariableDeclaration().getInitializer().has_value());
+        const auto* memberExpression = dynamic_cast<const IdentifierLikeExpressionNode*>(memberStatement->getVariableDeclaration().getInitializer().value().get());
+        ASSERT_NE(memberExpression, nullptr);
+        ASSERT_EQ(memberExpression->getGenericArguments().size(), 1);
+        const auto* memberArgument = dynamic_cast<const MemberAccessExpressionNode*>(memberExpression->getGenericArguments().front().get());
+        ASSERT_NE(memberArgument, nullptr);
+        EXPECT_NE(dynamic_cast<const IdentifierLikeExpressionNode*>(&memberArgument->getObject()), nullptr);
+        EXPECT_EQ(memberArgument->getMember().getName().getIdentifierString(), "value");
+
+        const auto* comparedStatement = dynamic_cast<const VariableDeclarationStatementNode*>(statements[2].get());
+        ASSERT_NE(comparedStatement, nullptr);
+        ASSERT_TRUE(comparedStatement->getVariableDeclaration().getInitializer().has_value());
+        const auto* comparedExpression = dynamic_cast<const BinaryExpressionNode*>(comparedStatement->getVariableDeclaration().getInitializer().value().get());
+        ASSERT_NE(comparedExpression, nullptr);
+        EXPECT_EQ(comparedExpression->getKind(), BinaryExpressionKind::LESS_THAN);
+        EXPECT_NE(dynamic_cast<const IdentifierLikeExpressionNode*>(&comparedExpression->getLeft()), nullptr);
+        EXPECT_NE(dynamic_cast<const IdentifierLikeExpressionNode*>(&comparedExpression->getRight()), nullptr);
+    }
+
+    TEST(ParserTest, ParsesGenericArgumentsForImportedTypeDeclarations) {
+        const auto* testInfo = testing::UnitTest::GetInstance()->current_test_info();
+        auto testDirectory = std::filesystem::temp_directory_path() / ("vnlctest-parser-" + std::string(testInfo->name()));
+        std::filesystem::remove_all(testDirectory);
+        std::filesystem::create_directories(testDirectory / "first_source");
+        std::filesystem::create_directories(testDirectory / "second_source");
+
+        std::ofstream firstModuleInterface(testDirectory / "first_source" / "models.vni");
+        firstModuleInterface << R"({"Box":{"category":"class","genericParameters":["T"],"properties":{},"methods":{},"baseClass":null,"implementedInterfaces":[],"final":false}})";
+        firstModuleInterface.close();
+
+        std::ofstream secondModuleInterface(testDirectory / "second_source" / "models.vni");
+        secondModuleInterface << R"({"Box":{"category":"interface","genericParameters":["T"],"methods":{}}})";
+        secondModuleInterface.close();
+
+        std::stringstream input(
+            "import first.models as firstModels\n"
+            "import second.models as secondModels\n"
+            "import first.models.Box as DirectBox\n"
+            "func test() {\n"
+            "    let first = firstModels.Box<Item>\n"
+            "    let second = secondModels.Box<Value>\n"
+            "    let direct = DirectBox<Member>\n"
+            "}\n"
+        );
+
+        Config config{
+            .mode = RunningMode::COMPILE,
+            .vanillangVersion = "1.0",
+            .minecraftVersion = "26.1.2",
+            .packageRootPath = testDirectory / "source_package",
+            .inputFilePath = testDirectory / "source_package" / "test.vnl",
+            .outputDirectory = std::nullopt,
+            .dependencyPackageRootPaths = {
+                { "first", testDirectory / "first_source" },
+                { "second", testDirectory / "second_source" },
+            },
+            .optimizationLevel = std::nullopt,
+        };
+
+        auto result = parseModule(input, config);
+        const auto& module = result.getModuleNode();
+        ASSERT_EQ(module.getTopIdentifierDeclarations().size(), 1);
+
+        const auto* function = dynamic_cast<const FunctionDeclarationNode*>(module.getTopIdentifierDeclarations().front().get());
+        ASSERT_NE(function, nullptr);
+        ASSERT_TRUE(function->getBody().has_value());
+
+        const auto& statements = function->getBody().value()->getStatements();
+        ASSERT_EQ(statements.size(), 3);
+
+        const auto firstStatement = dynamic_cast<const VariableDeclarationStatementNode*>(statements[0].get());
+        ASSERT_NE(firstStatement, nullptr);
+        ASSERT_TRUE(firstStatement->getVariableDeclaration().getInitializer().has_value());
+        const auto* firstExpression = dynamic_cast<const MemberAccessExpressionNode*>(firstStatement->getVariableDeclaration().getInitializer().value().get());
+        ASSERT_NE(firstExpression, nullptr);
+        EXPECT_EQ(firstExpression->getMember().getName().getIdentifierString(), "Box");
+        ASSERT_EQ(firstExpression->getMember().getGenericArguments().size(), 1);
+        const auto* firstArgument = dynamic_cast<const IdentifierLikeExpressionNode*>(firstExpression->getMember().getGenericArguments().front().get());
+        ASSERT_NE(firstArgument, nullptr);
+        EXPECT_EQ(firstArgument->getName().getIdentifierString(), "Item");
+
+        const auto secondStatement = dynamic_cast<const VariableDeclarationStatementNode*>(statements[1].get());
+        ASSERT_NE(secondStatement, nullptr);
+        ASSERT_TRUE(secondStatement->getVariableDeclaration().getInitializer().has_value());
+        const auto* secondExpression = dynamic_cast<const MemberAccessExpressionNode*>(secondStatement->getVariableDeclaration().getInitializer().value().get());
+        ASSERT_NE(secondExpression, nullptr);
+        EXPECT_EQ(secondExpression->getMember().getName().getIdentifierString(), "Box");
+        ASSERT_EQ(secondExpression->getMember().getGenericArguments().size(), 1);
+        const auto* secondArgument = dynamic_cast<const IdentifierLikeExpressionNode*>(secondExpression->getMember().getGenericArguments().front().get());
+        ASSERT_NE(secondArgument, nullptr);
+        EXPECT_EQ(secondArgument->getName().getIdentifierString(), "Value");
+
+        const auto directStatement = dynamic_cast<const VariableDeclarationStatementNode*>(statements[2].get());
+        ASSERT_NE(directStatement, nullptr);
+        ASSERT_TRUE(directStatement->getVariableDeclaration().getInitializer().has_value());
+        const auto* directExpression = dynamic_cast<const IdentifierLikeExpressionNode*>(directStatement->getVariableDeclaration().getInitializer().value().get());
+        ASSERT_NE(directExpression, nullptr);
+        EXPECT_EQ(directExpression->getName().getIdentifierString(), "DirectBox");
+        ASSERT_EQ(directExpression->getGenericArguments().size(), 1);
+        const auto* directArgument = dynamic_cast<const IdentifierLikeExpressionNode*>(directExpression->getGenericArguments().front().get());
+        ASSERT_NE(directArgument, nullptr);
+        EXPECT_EQ(directArgument->getName().getIdentifierString(), "Member");
+
+        std::error_code error;
+        std::filesystem::remove_all(testDirectory, error);
     }
 
 } // namespace vnlc
